457e4fc2c7807f6531d68022ca2f6a3dacc2c7cd
Hambuch Konrád <konrad.hambuch@gmail.com>
2022-01-06 09:30:55 +0100

    initial commit

--- src/main/java/com/jmeter/demo/App.java
+++ src/main/java/com/jmeter/demo/App.java
@@ -1,6 +1,6 @@
 package com.jmeter.demo;
 import org.hyperledger.fabric.gateway.*;
-import org.hyperledger.fabric.gateway.spi.WalletStore;
+import org.hyperledger.fabric.sdk.Channel;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -14,29 +14,27 @@ public class App
     {
         try{
             String pathRoot = "";
-            String walletName = "wallet\\Admin@fi.example.com";
+            String walletName = "wallet";
             Path walletDirectory = Paths.get(pathRoot + walletName);
             System.out.println(walletDirectory.toAbsolutePath().toString());
             //Path walletPath_2 = Paths.get(pathRoot + "org-2-wallet");
-            Path networkConfigFile = Paths.get(pathRoot + "notls\\connection.json");
+            Path networkConfigFile = Paths.get(pathRoot + "notls/connection.json");
             //Path networkConfigPath_2 = Paths.get(pathRoot + "2-Org-Local-Fabric-Org2_connection.json");
             // Load an existing wallet holding identities used to access the network.
             //Path walletDirectory = Paths.get("resources/connection/wallet");
-            Wallet wallet = Wallets.newFileSystemWallet(walletDirectory);
-
+            Wallet wallet = Wallet.createFileSystemWallet(walletDirectory);
             // Path to a common connection profile describing the network.
             //Path networkConfigFile = Paths.get("resources/connection/connection.json");
-
+            System.out.println(wallet.getAllLabels());
             // Configure the gateway connection used to access the network.
             Gateway.Builder builder = Gateway.createBuilder()
-                    .identity(wallet, "Admin")
+                    .identity(wallet, "Admin@fi.example.com")
                     .networkConfig(networkConfigFile);
 
             // Create a gateway connection
             try (Gateway gateway = builder.connect()) {
-
                 // Obtain a smart contract deployed on the network.
-                Network network = gateway.getNetwork("e-pengo-channel");
+                Network network = gateway.getNetwork("epengo-channel");
                 Contract contract = network.getContract("cbdc");
 
                 // Submit transactions that store state to the ledger.
